6dbba9a814b0a280d47deef6befcf827887c80c9
ruswerner <rus.werner@gmail.com>
2013-05-04 14:21:11 +1200

    Formatted code according to style guide. Removed google closure relic. Tests pass.

--- src/helpers.js
+++ src/helpers.js
@@ -6,88 +6,86 @@
 
 // Add transform and transformX functions for an OT type which has transformComponent defined.
 // transformComponent(destination array, component, other component, side)
-exports._bootstrapTransform = function(type, transformComponent, checkValidOp, append) {
+exports._bootstrapTransform = function (type, transformComponent, checkValidOp, append) {
 
-    var transformComponentX = function(left, right, destLeft, destRight) {
-        transformComponent(destLeft, left, right, 'left');
-        transformComponent(destRight, right, left, 'right');
-    };
+  var transformComponentX = function (left, right, destLeft, destRight) {
+    transformComponent(destLeft, left, right, 'left');
+    transformComponent(destRight, right, left, 'right');
+  };
 
-    // Transforms rightOp by leftOp. Returns ['rightOp', clientOp']
-    var transformX = function(leftOp, rightOp) {
-        checkValidOp(leftOp);
-        checkValidOp(rightOp);
+  // Transforms rightOp by leftOp. Returns ['rightOp', clientOp']
+  var transformX = function (leftOp, rightOp) {
+    checkValidOp(leftOp);
+    checkValidOp(rightOp);
 
-        var newRightOp = [];
+    var newRightOp = [];
 
-        for(var i = 0; i < rightOp.length; i++) {
-            var rightComponent = rightOp[i];
+    for (var i = 0; i < rightOp.length; i++) {
+      var rightComponent = rightOp[i];
 
-            // Generate newLeftOp by composing leftOp by rightComponent
-            var newLeftOp = [];
+      // Generate newLeftOp by composing leftOp by rightComponent
+      var newLeftOp = [];
 
-            var k = 0;
-            while(k < leftOp.length) {
-                var nextC = [];
-                transformComponentX(leftOp[k], rightComponent, newLeftOp, nextC);
-                k++;
+      var k = 0;
+      while (k < leftOp.length) {
+        var nextC = [];
+        transformComponentX(leftOp[k], rightComponent, newLeftOp, nextC);
+        k++;
 
-                if(nextC.length === 1) {
-                    rightComponent = nextC[0];
-                } else if(nextC.length === 0) {
-                    var tmp = leftOp.slice(k);
-                    for(var j = 0; j < tmp.length; j++) {
-                        append(newLeftOp,tmp[j]);
-                    }
-                    rightComponent = null;
-                    break;
-                } else {
-                    // Recurse
-                    var ret = transformX(leftOp.slice(k), nextC);
-                    var l_ = ret[0];
-                    var r_ = ret[1];
-                    for(var j = 0; j < l_.length; j++) {
-                        append(newLeftOp,l_[j]);
-                    }
-                    for(var j = 0; j < r_.length; j++) {
-                        append(newRightOp,r_[j]);
-                    }
-                    rightComponent = null;
-                    break;
-                }
-            }
-
-            if(rightComponent != null) {
-                append(newRightOp, rightComponent);
-            }
-
-            leftOp = newLeftOp;
-        }
-
-        return [leftOp, newRightOp];
-    };
-
-    type.transformX = type['transformX'] = transformX;
-
-    var transform = function(op, otherOp, type) {
-        if(type !== 'left' && type !== 'right') {
-            throw new Error("type must be 'left' or 'right'");
-        }
-
-        if(otherOp.length === 0)
-            return op;
-
-        if (op.length === 1 && otherOp.length === 1) {
-            return transformComponent([], op[0], otherOp[0], type);
-        }
-
-        if (type === 'left') {
-            return transformX(op, otherOp)[0];
+        if (nextC.length === 1) {
+          rightComponent = nextC[0];
+        } else if (nextC.length === 0) {
+          var tmp = leftOp.slice(k);
+          for (var j = 0; j < tmp.length; j++) {
+            append(newLeftOp, tmp[j]);
+          }
+          rightComponent = null;
+          break;
         } else {
-            return transformX(otherOp, op)[1];
+          // Recurse
+          var ret = transformX(leftOp.slice(k), nextC);
+          var l_ = ret[0];
+          var r_ = ret[1];
+          for (var j = 0; j < l_.length; j++) {
+            append(newLeftOp, l_[j]);
+          }
+          for (var j = 0; j < r_.length; j++) {
+            append(newRightOp, r_[j]);
+          }
+          rightComponent = null;
+          break;
         }
-    };
+      }
 
-    type.transform = type['transform'] = transform;
+      if (rightComponent != null) {
+        append(newRightOp, rightComponent);
+      }
+
+      leftOp = newLeftOp;
+    }
+
+    return [leftOp, newRightOp];
+  };
+
+  type.transformX = transformX;
+
+  type.transform = function (op, otherOp, type) {
+    if (type !== 'left' && type !== 'right') {
+      throw new Error("type must be 'left' or 'right'");
+    }
+
+    if (otherOp.length === 0)
+      return op;
+
+    if (op.length === 1 && otherOp.length === 1) {
+      return transformComponent([], op[0], otherOp[0], type);
+    }
+
+    if (type === 'left') {
+      return transformX(op, otherOp)[0];
+    } else {
+      return transformX(otherOp, op)[1];
+    }
+  };
 
 };
